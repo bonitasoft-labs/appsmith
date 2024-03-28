dd510d5e04aceec729f6873059b5f3bccca9d2f8
benjaminparisel <benjamin.parisel@bonitasoft.com>
2024-03-28 16:20:00 +0100

    chore: Improve build local script

--- build_local.sh
+++ build_local.sh
@@ -1,24 +1,38 @@
+#!/bin/bash
+set -euxo pipefail
+
+WORKDIR=$(pwd)
+
 # Prerequisite:
 #   server: Java 17
 #   client: Node 18
 #   generate_info_json.sh: jq
 #   docker of course
-cd app/server
-./build.sh -DskipTests &
 
-cd ../client
+# Get the start time
+start_time=$(date +%s)
 
-yarn && yarn build &
+cd $WORKDIR/app/server
+./build.sh -DskipTests
 
-cd packages/rts
+cd $WORKDIR/app/client
 
-yarn && yarn build &
+yarn && yarn build
 
-cd ../../../..
+cd $WORKDIR/app/client/packages/rts
 
+yarn && yarn build
+
+cd $WORKDIR
 scripts/generate_info_json.sh
 
 wait
 
 echo "## Build docker image"
 docker build --build-arg "BASE=appsmith/base-ce:release" -t bonitasoft/appsmith-ce:latest .
+
+end_time=$(date +%s)
+execution_time=$((end_time - start_time))
+
+echo "Execution time: $execution_time seconds"
+echo "All commands completed successfully"
