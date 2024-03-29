1be0af5916d5aed79df2c8da56bae37d07ac85a9
Benjamin Parisel <benjamin.parisel@bonitasoft.com>
2024-03-29 16:21:41 +0100

    chore: Improve build local script (#3)
    * waiting for thread ending before build docker image
    * make executable

--- build_local.sh
+++ build_local.sh
@@ -1,25 +1,38 @@
+#!/bin/bash
+set -euxo pipefail
+
+WORKDIR=$(pwd)
+
 # Prerequisite:
 #   server: Java 17
 #   client: Node 18
 #   generate_info_json.sh: jq
 #   docker of course
-cd app/server
-mvn clean compile -DskipTests
 
+# Get the start time
+start_time=$(date +%s)
+
+cd $WORKDIR/app/server
 ./build.sh -DskipTests
 
-cd ../client
+cd $WORKDIR/app/client
 
-yarn install
-yarn build
+yarn && yarn build
 
-cd packages/rts
+cd $WORKDIR/app/client/packages/rts
 
-yarn install
-yarn build
+yarn && yarn build
 
-cd ../../../..
-
+cd $WORKDIR
 scripts/generate_info_json.sh
 
-docker build --build-arg "BASE=appsmith/base-ce:release" -t appsmith/appsmith-ce:latest .
+wait
+
+echo "## Build docker image"
+docker build --build-arg "BASE=appsmith/base-ce:release" -t bonitasoft/appsmith-ce:latest .
+
+end_time=$(date +%s)
+execution_time=$((end_time - start_time))
+
+echo "Execution time: $execution_time seconds"
+echo "All commands completed successfully"
